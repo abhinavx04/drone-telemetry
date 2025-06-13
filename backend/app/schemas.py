c1711d189fcf497d8edfc33f0a487c326f3b0080
kshitij <kpandey0022@gmail.com>
2025-06-13 14:41:50 +0530

    schemas.py= changed timestamp to datatime

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 class TelemetryIn(BaseModel):
     drone_id: str = Field(..., max_length=50)
     latitude: float
     longitude: float
     absolute_altitude_m: float | None = None
-    timestamp: int
+    timestamp: datetime
 
 class TelemetryOut(TelemetryIn):
     id: int
